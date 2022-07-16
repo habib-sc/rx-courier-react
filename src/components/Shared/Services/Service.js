55d343e697b0b324b4cc5cc815fa274b9cf31595
Habibur Rahman <habib97.cs@gmail.com>
2022-07-16 13:57:01 +0600

    warning fix in service.js

--- src/components/Shared/Services/Service.js
+++ src/components/Shared/Services/Service.js
@@ -1,9 +1,9 @@
 
 const Service = ({service}) => {
-    const id = service.id;
+    const id = parseInt(service.id);
     return (
         
-        <div className={`${id == 2 || id == 5 || id == 8 ? "-mt-16": ""} ${id == 3 || id == 6 || id == 9 ? "-mt-32": ""}`} data-aos="fade-up-left" data-aos-duration="1000">
+        <div className={`${id === 2 || id === 5 || id === 8 ? "-mt-16": ""} ${id === 3 || id === 6 || id === 9 ? "-mt-32": ""}`} data-aos="fade-up-left" data-aos-duration="1000">
             <div className={`card rounded-lg my-4 bg-base-100 shadow-xl hover:scale-110 hover:bg-primary hover:text-white transition ease-in-out delay-150 duration-300`}>
                 <figure><img src={service.img} className="w-full max-h-80" alt="Shoes" /></figure>
                 <div className="card-body">
